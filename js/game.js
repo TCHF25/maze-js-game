6c10cb165218fe6185dfac786b0454adce70f1dd
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-09-03 17:33:31 +0300

    javascript code upgrade

---
+++ js/game.js
@@ -0,0 +1,27 @@
+function fct1(){
+    const reset = document.getElementById("start");
+    const finish_line = document.getElementById("end");   
+    const walls = document.getElementById("boundary1");
+    const result = document.getElementById("status");
+
+
+    
+
+
+
+    // //
+    // if  clicked, the maze will restart, ya3ne walls wont be red//
+
+    //if maze not comleted display you  lost on result, if not you won//
+    // 
+
+
+
+
+
+    
+
+
+}
+
+fct1();
