b732f9a70a02b2076f84053695f39ec6d5bea655
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-09-04 00:42:52 +0300

    add eventListener

--- js/game.js
+++ js/game.js
@@ -1,30 +1,51 @@
-//var boundaries = document.getElementsByClassName("boundary");
+window.addEventListener('load', () => {
+
+ var boundaries = document.querySelectorAll('.boundary')
+ var statu = document.getElementById("status");
+ var startLine = document.getElementById("start");
+ var endLine = document.getElementById("end");
 
 
+    boundaries.forEach((item) => {
+    (item).addEventListener("mouseover", () => {
+        (item).style.backgroundColor = "red";
+        statu.textContent = "You Lost"   
 
-document.querySelectorAll('.boundary').forEach(item => {
-    item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "red";
         });
-    })
-  
+    });
+
+    
+    endLine.addEventListener("mouseover", () => {
+        statu.textContent = "You Won"   
+    });
 
 
-   
+    startLine.addEventListener("click", () => {
+      
+      boundaries.forEach((item) => {
+        item.style.backgroundColor = "#eeeeee";
+        statu.textContent = "Begin by moving your mouse over the 'S'";
+    });
+   });
 
- 
 
-     
+    //let counts=setInterval(updated);
+       // let upto=1000;
+        //function updated(){
+           // var count= document.getElementById("counter");
+            //count.innerHTML=--upto;
+           // if(upto === 0) {
+           //     clearInterval(counts);
+           // }
+       // }
 
 
 
-
+});
 
 //getElementsByClassName returns an array of elements,
 // addEventListener exists on elements.
 //The fix would be to iterate over the result set from getElementsByClassName
 // and call addEventListener on each item:
 
-//reset.addEventListener("click", () => {
-      //   limit bterja3 nafs el  normal background color 
-      //  and it restarts the score to 0
+// keep score , win = +5 lose = -10
