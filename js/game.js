526b4120f280bf4928898e85275732a3e02715b2
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-09-04 21:32:08 +0300

    remove useless code

--- js/game.js
+++ js/game.js
@@ -5,35 +5,15 @@ window.addEventListener('load', () => {
  var startLine = document.getElementById("start");
  var endLine = document.getElementById("end");
  var stuck = document.getElementById("game");
- //var score = document.getElementsByClassName(".boundary.exemple");
  var scoreText;
 
 
-//if didnt hover over s game doesnt start.
-
-   
-
-    function gamestart () {
-        startLine.addEventListener("mouseover");
-    };
-
-
-
-
-
-//entered game  and cant go out or you lose
     stuck.addEventListener("mouseleave", gameover);
 
-
-
- //game lost fucntion
- 
 
     boundaries.forEach((item) => {
     (item).addEventListener("mouseover", gameover)
     });
-
-
 
 
     function gameover () {
@@ -44,7 +24,6 @@ window.addEventListener('load', () => {
      });
     };
 
-    //gamewin function
     
     endLine.addEventListener("mouseleave", gamewon);
     
@@ -53,26 +32,6 @@ window.addEventListener('load', () => {
         stuck.removeEventListener("mouseleave", gameover);  
         statu.textContent = "You Won" 
     };
-
-
-
-   // endLine.addEventListener("mouseover", () => {
-    //    (item).style.backgroundColor = "red";
-    //    statu.textContent = "You Lost"  
-
-
-
-
-
-
-
- 
-    
-    //endLine.addEventListener("mouseover", () => {
-     //   statu.textContent = "You Won"   
-   // });
-
-
 
 
     startLine.addEventListener("click", () => {
@@ -90,32 +49,10 @@ window.addEventListener('load', () => {
    scoreText = stuck.appendChild(5, 5, 'Points: 0', { font: '18px Arial', fill: '#0095DD' });
    }
 
-   
-
   //function addfive() {
     //brick.kill();
     //score += 5;
     //scoreText.setText(`Points: ${score}`);
     //};
 
-   
-
-
-
-
-
-
 });
-
-
-
-
-
-
-
-//getElementsByClassName returns an array of elements,
-// addEventListener exists on elements.
-//The fix would be to iterate over the result set from getElementsByClassName
-// and call addEventListener on each item:
-
-// keep score , win = +5 lose = -10
